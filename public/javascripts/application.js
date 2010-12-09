100e12e23d4a48c2da6405e6acb1141f265469a7
Rufus Post <rufuspost@gmail.com>
2010-12-09 21:50:27 +1100

    async loading of js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,59 +1,56 @@
-$('html').addClass('js');
+$(document).ready(function() {
+  $('article').fadeIn();
+});
 
-$(document).ready(function($) {
-  //FOUC
-  $('h1').show();
+$(".input.date input").datepicker();
 
-  $(".input.date input").datepicker({});
-  // We only want these styles applied when javascript is enabled
-  $('div.navigation').css({'width' : '200px', 'float' : 'left'});
-  $('div.content').css('display', 'block');
+$('div.navigation').css({
+  'width' : '200px', 'float' : 'left'
+});
 
-  // Initially set opacity on thumbs and add
-  // additional styling for hover effect on thumbs
-  var onMouseOutOpacity = 0.67;
-  $('#thumbs ul.thumbs li').opacityrollover({
-    mouseOutOpacity:   onMouseOutOpacity,
-    mouseOverOpacity:  1.0,
-    fadeSpeed:         'fast',
-    exemptionSelector: '.selected'
-  });
-  
-  // Initialize Advanced Galleriffic Gallery
-  var gallery = $('#thumbs').galleriffic({
-    delay:                     2500,
-    numThumbs:                 10,
-    preloadAhead:              10,
-    enableTopPager:            true,
-    enableBottomPager:         true,
-    maxPagesToShow:            7,
-    imageContainerSel:         '#slideshow',
-    controlsContainerSel:      '#controls',
-    captionContainerSel:       '#caption',
-    loadingContainerSel:       '#loading',
-    renderSSControls:          true,
-    renderNavControls:         true,
-    playLinkText:              'Play Slideshow',
-    pauseLinkText:             'Pause Slideshow',
-    prevLinkText:              '&lsaquo; Previous Photo',
-    nextLinkText:              'Next Photo &rsaquo;',
-    nextPageLinkText:          'Next &rsaquo;',
-    prevPageLinkText:          '&lsaquo; Prev',
-    enableHistory:             false,
-    autoStart:                 false,
-    syncTransitions:           true,
-    defaultTransitionDuration: 900,
-    onSlideChange:             function(prevIndex, nextIndex) {
-      // 'this' refers to the gallery, which is an extension of $('#thumbs')
-      this.find('ul.thumbs').children()
-        .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
-        .eq(nextIndex).fadeTo('fast', 1.0);
-    },
-    onPageTransitionOut:       function(callback) {
-      this.fadeTo('fast', 0.0, callback);
-    },
-    onPageTransitionIn:        function() {
-      this.fadeTo('fast', 1.0);
-    }
-  });
+$('div.content').css('display', 'block');
+
+var onMouseOutOpacity = 0.67;
+
+$('#thumbs ul.thumbs li').opacityrollover({
+  mouseOutOpacity:   onMouseOutOpacity,
+  mouseOverOpacity:  1.0,
+  fadeSpeed:         'fast',
+  exemptionSelector: '.selected'
+});
+
+var gallery = $('#thumbs').galleriffic({
+  delay:                     2500,
+  numThumbs:                 10,
+  preloadAhead:              10,
+  enableTopPager:            true,
+  enableBottomPager:         true,
+  maxPagesToShow:            7,
+  imageContainerSel:         '#slideshow',
+  controlsContainerSel:      '#controls',
+  captionContainerSel:       '#caption',
+  loadingContainerSel:       '#loading',
+  renderSSControls:          true,
+  renderNavControls:         true,
+  playLinkText:              'Play Slideshow',
+  pauseLinkText:             'Pause Slideshow',
+  prevLinkText:              '&lsaquo; Previous Photo',
+  nextLinkText:              'Next Photo &rsaquo;',
+  nextPageLinkText:          'Next &rsaquo;',
+  prevPageLinkText:          '&lsaquo; Prev',
+  enableHistory:             false,
+  autoStart:                 false,
+  syncTransitions:           true,
+  defaultTransitionDuration: 900,
+  onSlideChange:             function(prevIndex, nextIndex) {
+    this.find('ul.thumbs').children()
+      .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
+      .eq(nextIndex).fadeTo('fast', 1.0);
+  },
+  onPageTransitionOut:       function(callback) {
+    this.fadeTo('fast', 0.0, callback);
+  },
+  onPageTransitionIn:        function() {
+    this.fadeTo('fast', 1.0);
+  }
 });
